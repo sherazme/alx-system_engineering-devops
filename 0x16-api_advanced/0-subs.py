98d57ff9e61169d4904c488357cd3402ec749325
Shiraz <you@example.com>
2024-05-07 18:57:08 -0700

    api_advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""queries the Reddit API and returns the number of subscribers"""
+
+
+def number_of_subscribers(subreddit):
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        return 0
+
+    return sub_info.json().get("data").get("subscribers")

bc036a5a25a2f6aa8785a765fae9da3c6ab9567e
Shiraz Mohammed Elhassan Osman Elmahi <73830525+sherazme@users.noreply.github.com>
2024-06-08 05:09:21 +0400

    Delete 0x1B-web_stack_debugging_4 directory

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++
@@ -1,3 +0,0 @@
-#puppet
-exec { '/usr/bin/env sed -i s/15/1000/ /etc/default/nginx': }
--> exec { '/usr/bin/env service nginx restart': }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++
@@ -1,7 +0,0 @@
-# Change OS configuration to let holberton user login
-# and open a file without any error message
-
-exec {'OS security config':
-  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
-  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
-}

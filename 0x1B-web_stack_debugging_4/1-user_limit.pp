ad13cfba952cb9a69474899643ef85681dd3ace1
root <you@example.com>
2024-06-26 18:48:51 -0700

    debugging

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,5 @@
 #change OS configuration to let holberton user login
 #and open file without any error message
 
-exec {'OS security config':
-  command => 'sed -i "s/holberton/foo" /etc/security/limits/conf',
-  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
 }

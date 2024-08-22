6d519b57d5be0f2c0713d624ae0fd959a737fd99
Shiraz Mohammed Elhassan Osman Elmahi <73830525+sherazme@users.noreply.github.com>
2024-08-22 02:19:23 +0200

    Update

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,4 @@
-#change OS configuration to let holberton user login
+#change configuration to let holberton user login
 #and open file without any error message
 
 exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
